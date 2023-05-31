354d8dcb9e95ddc45fd4db9bff2ef18acd3ec3d7
Antoine Aube <aaube3@gmail.com>
2023-05-31 13:31:37 -0600

    adds .shift method to all arrays

--- arrays.rb
+++ arrays.rb
@@ -10,3 +10,13 @@ slc_spring_temperatures = [55.5, 78.1, 87.7, 67.7, 65.4, 74.8]
 #array of booleans
 array_of_booleans = [true, true, false, true, false, false, true]
 
+# calling the .shift method will remove and return the first element in the array. 
+# .shift(arg) will remove the number of elements given in the argunment 
+# starting from the beginning of the array, and return as a new array
+# I used p instead of puts so I could see the output as an array in my terminal
+
+p famous_cities.shift 
+p significant_years.shift(2)
+p slc_spring_temperatures.shift(4)
+p array_of_booleans.shift
+
