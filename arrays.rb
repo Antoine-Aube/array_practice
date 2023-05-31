b45681d7228d21f7756d41389143a91e97355b5a
Antoine Aube <aaube3@gmail.com>
2023-05-31 13:24:02 -0600

    add arrays with different data types

---
+++ arrays.rb
@@ -0,0 +1,12 @@
+#array of strings
+famous_cities = ["Rome", "Paris", "New York City", "Tokyo"]
+
+#array of integers
+significant_years = [1991, 1999, 2009, 2015]
+
+#array of floats
+slc_spring_temperatures = [55.5, 78.1, 87.7, 67.7, 65.4, 74.8]
+
+#array of booleans
+array_of_booleans = [true, true, false, true, false, false, true]
+
